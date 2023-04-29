84309749ad4d9f22ac21110a894f7632dfbd5e9a
Richard Langner <31290551+RichardLangner@users.noreply.github.com>
2023-04-29 09:41:29 +0100

    WIP to get flashes right

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ Written by Richard Langner, Sheffield Hackspace, UK 23 April 2023.
 #include "SimpleTimer.h"					// Include the Timer class definition.
 void flashDigit(int num);
 int ledPin = D4;                            // Wemos D1 mini
-int flashCounter =0;
-int arrayPointer=0;
+#define DEBUG Serial.printf("%3d P = %2d, digit = %d, F = %d, LED will be %s for ms = %4d ms\n", __LINE__, arrayPointer, array[arrayPointer], flashCounter, digitalRead(ledPin) ? "Off":"On ", ms);
+
 
 void setup() {
 	Serial.begin(74880);
@@ -17,48 +17,55 @@ void setup() {
 }
 
 void loop() {
-	flashDigit(135);
+	flashDigit(241);
 }
 
 void flashDigit(int num){
     static SimpleTimer timer1;
     static int ms = 200;
+    static int flashCounter =0;
+    static int arrayPointer=0;
 
-	if (!timer1.done(ms, 0)){ return;}							// Return if nothing to do; starts timer if not running.
+    // Return if nothing to do; starts timer if not already running.
+	if (!timer1.done(ms, 0)){ return;}
 
-    int array[] {0, (num/100)%10, 0, (num/10)%10, 0, num %10, 0};  // H,T,U and interdigit indicator
-
-    if(!(arrayPointer %2)){       // Gap between digits and a start
-        digitalWrite(ledPin,1); // Turn off LED if array pointer is zero or even
-        ms = 1000;
-        ++arrayPointer %=7;
-        flashCounter=0;
+    // H,T,U and interdigit indicators
+    int array[] {0, (num/100)%10, 0, (num/10)%10, 0, num %10, 0};
+    
+    // Array pointer is even, or zero?
+    if(!(arrayPointer %2)){         // If pointer is even 0,2,4,6...
+        digitalWrite(ledPin,1);     // Turn LED off
+        ms = 1000;                  // for 1000ms
+        ++arrayPointer %=7;         // Next array element (or wrap around to zero)
+        flashCounter=0;             // Reset flash counter
+        DEBUG
         return;
     }
-    
-    digitalWrite(ledPin, (!((flashCounter) %2) ? HIGH:LOW));        // Odd count = LOW (LED on)
-    flashCounter++; Serial.printf("%3d arrayPointer = %2d, num = %d, ms = %3d, flashCounter = %d\n", __LINE__, arrayPointer, array[arrayPointer], ms, flashCounter);
+
+    // Change the LED state; if flashCounter is 'even' turn off LED
+    flashCounter++; 
+    digitalWrite(ledPin, flashCounter %2 ? HIGH:LOW);        // Odd count = LOW (LED on)
 
     // If the digit value is zero
     if(0 == array[arrayPointer]){
-        ms = 1000;
-        if(!flashCounter %2){            // If even (LED was off)
-            (++arrayPointer)  %=7;       // Reset
-            flashCounter = 0;
-        }
-        Serial.printf("%3d arrayPointer = %2d, num = %d, ms = %2d, flashCounter = %d\n", __LINE__, arrayPointer, array[arrayPointer], ms, flashCounter);
+        digitalWrite(ledPin,0);
+        flashCounter = 0;
+        ++arrayPointer %=7;
+        ms = 1000;        
+        DEBUG
         return;
     }
 
-    // Digit is non-zero. Do we need more flashes?
+    // Digit value is non-zero. Either get more flashes, or get next digit
     if(flashCounter < array[arrayPointer] *2 ){
-    ms = 200;
-    Serial.printf("%3d arrayPointer = %2d, num = %d, ms = %3d, flashCounter = %d\n", __LINE__, arrayPointer, array[arrayPointer], ms, flashCounter);
-    return;
-    } 
-    else
-    {
-         ++arrayPointer %=7;
+        ms = 200;
+        DEBUG
     return;
     }
+
+    else {
+        ++arrayPointer %=7;
+        ms=1000; 
+        DEBUG
+    return; }
 }
