960c31f70811fc89ef564ab1e87f53830314fa8d
Richard Langner <31290551+RichardLangner@users.noreply.github.com>
2023-05-21 18:29:17 +0100

    FlashDigits is now a class. LED pin and active level are set

--- include/flashNumber.h
+++ include/flashNumber.h
@@ -4,70 +4,88 @@ Repeats after 3000ms. Returns 'true' when it completes a group of digits.
 The flash 'on' and 'off' period is 200ms each. A zero is 1000ms 'on'.
 Interdigit gap is defined by a negative number of ms.
 Written by Richard Langner, Sheffield Hackspace, UK 21 May 2023.
-Latest code on github.com/RichardLangner/SimpleTimer
+Latest code and examples on github.com/RichardLangner/SimpleTimer
 */
 #ifndef FLASHNUMBER
 #define FLASHNUMBER
 #include <Arduino.h>
-
 #include "SimpleTimer.h"
-#define DEBUG Serial.printf("%3d After %4d ms of LED %s, num=%d, P=%d, digit=%d, F=%d, offset=%d\n", __LINE__, ms, digitalRead(ledPin) ? "Off":"On ", num, arrayPointer, array[arrayPointer], flashCounter, offset);
-#define DEBUG2 Serial.printf("%3d LED=%s\n", __LINE__, digitalRead(ledPin) ? "Off":"On ");
 
 /** @return - true if all the digits have been flashed.
 *	@param num number to flash, max 99999.
-*	@param wide number of digit places to flash, 0 means automatic with no leading zeros.
 *   @param ledPin GPIO port number of the LED.
-*   @param on logic level required for LED to be on.
+*   @param LED_on logic level required for LED to be on.
+*	@param width (opional) number of digit places to flash. If not specified, or zero, means with no leading zeros.
 */
 
-bool flashNumber(int num, int wide, int ledPin, bool on){
-    static SimpleTimer timer1;
-    static int ms = 10;
-    static int flashCounter =0;
-    static int arrayPointer;
-    static int offset;
+class FlashDigits
+{
+private:
+bool    _enabled=true;
+int     _ledPin, _led_on;
 
-    // Return if nothing to do; starts timer if not already running.
-	if (!timer1.done(ms, 0)){return false;}
-    // Validate arguments
-    if(num <0 or wide <0 or wide >5) {return false;}
+public:
+virtual ~ FlashDigits(){}
 
-    // Digit values and interdigit times
-    int array[] {-500, (num/10000)%10, -500, (num/1000)%10,-500, (num/100)%10, -500, (num/10)%10, -500, num %10, -2500};
+    bool flash(int num, int ledPin, bool LED_on, int width=0){
+        static SimpleTimer timer1;
+        static int ms = 10, flashCounter =0, arrayPointer, offset;
 
-    // Calculate the offset array position to start at
-    if(wide==0){offset= 10-(2 * ((int)log10(num) +1));} else {offset = 10 - 2*wide;}
-    if(arrayPointer==0) {arrayPointer= offset;}DEBUG
-    
-    // Gaps between digits (indicated by a minus value)
-    if(array[arrayPointer] < 0){                // Inter-digit gap will be less than 0
-        digitalWrite(ledPin, !on);              // Turn LED off
-        ms = -array[arrayPointer];              // Make value positive
-        flashCounter=0; ++arrayPointer %=11;    // Next array element (or wrap around to zero)
-        if(arrayPointer==0) {return true;}
-        return false;
+        // Exit if disabled
+        if(!_enabled){return false;}
+        // Return if nothing to do; starts timer if not already running.
+        if (!timer1.done(ms, 0)){return false;}
+        // Validate arguments
+        if(num <0 or width <0 or width >5) {return false;}
+
+        // Digit values and interdigit times
+        int array[] {-500, (num/10000)%10, -500, (num/1000)%10,-500, (num/100)%10, -500, (num/10)%10, -500, num %10, -2500};
+
+        // Calculate the offset array position to start at
+        if(width==0){offset= 10-(2 * ((int)log10(num) +1));} else {offset = 10 - 2*width;}
+        if(arrayPointer==0) {arrayPointer= offset;}
+        
+        // Gaps between digits (indicated by a minus value)
+        if(array[arrayPointer] < 0){                // Check if minus
+            digitalWrite(ledPin, !LED_on);          // Turn LED off
+            ms = -array[arrayPointer];              // Make gap value positive
+            flashCounter=0; ++arrayPointer %=11;    // Next array element (or wrap around to zero)
+            if(arrayPointer==0) {return true;}
+            return false;
+        }
+
+        // Change the LED state; if flashCounter is 'even' turn off LED
+        flashCounter++; 
+        digitalWrite(ledPin, flashCounter %2 ? LED_on : !LED_on);  // Odd count = on
+
+        // If the digit value is zero
+        if(0 == array[arrayPointer]){
+            digitalWrite(ledPin,LED_on);
+            ms = 1000;
+            flashCounter = 0; ++arrayPointer %=11;
+            if(arrayPointer==0) {return true;}
+            return false;
+        }
+
+        // Digit value is non-zero. Either get more flashes, or get next digit
+        if(flashCounter < array[arrayPointer] *2 ){ms = 200; return false;
+        } else {  // Get next digit
+            flashCounter = 0; ++arrayPointer %=11;
+            if(arrayPointer==0) {return true;}  
+            return false; 
+        }
     }
 
-    // Change the LED state; if flashCounter is 'even' turn off LED
-    flashCounter++; 
-    digitalWrite(ledPin, flashCounter %2 ? on : !on);  // Odd count = on
+/**	@return 'true' if the flasher is enabled */
+    bool enabled(){return _enabled;}
+/** @param enabled 'true' enables, 'false' disables the flasher */   
+    void enabled(bool enabled){_enabled=enabled;}
 
-    // If the digit value is zero
-    if(0 == array[arrayPointer]){
-        digitalWrite(ledPin,on);
-        ms = 1000;
-        flashCounter = 0; ++arrayPointer %=11;
-        if(arrayPointer==0) {return true;}
-        return false;
-    }
-
-    // Digit value is non-zero. Either get more flashes, or get next digit
-    if(flashCounter < array[arrayPointer] *2 ){ms = 200; return false;
-    } else {  // Get next digit
-        flashCounter = 0; ++arrayPointer %=11;
-        if(arrayPointer==0) {return true;}  
-        return false; 
-    }
-}
+/** @return Nothing returned
+    @param  ledPin the LED pin number
+    @param  level logic level to turn on the LED**/
+    void setup(int ledPin, int led_on){
+        _ledPin=ledPin; _led_on=led_on;
+    }   
+};
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,17 @@ Blink (up to) a 5 digit number on a single LED, with 500ms gap between each digi
 Returns 'true' when it completes a group of digits, then repeats after several seconds. 
 The flash 'on' and 'off' period is 200ms each. A zero is represented by 1000ms 'on'.
 
+FlashDigits is a class, so you can have several independent flashers at once.
+To create a Flashdigits object called 'flasher1':
+   FlashDigits flasher1;  
+
 Written by Richard Langner, Sheffield Hackspace, UK 21 May 2023.
 Latest code and examples on github.com/RichardLangner/SimpleTimer
 */
 
 #include "SimpleTimer.h"			// github.com/RichardLangner/SimpleTimer
 #include "flashNumber.h"            // github.com/RichardLangner/SimpleTimer/tree/main/SimpleTimer_examples/
+FlashDigits flasher1;                // Create an instance of a flasher object
 
 int ledPin = D4;                    // Wemos D1 mini LED pin, active LOW
 int counter=0;                      // How many time the complete number flashes
@@ -18,10 +23,16 @@ void setup() {
 	pinMode(ledPin, OUTPUT);        // Set LED pin to be an output
     digitalWrite(ledPin, HIGH);     // LED is active LOW
     delay(3000);                    // Not required
+    flasher1.setup(ledPin, LOW);    // Define the LED pin and its active level
+    Serial.printf( "Flasher enabled = %s\n", (flasher1.enabled() ? "Yeh": "Nope"));
 }
 
 void loop() {
-    if(flashNumber(counter, 0, ledPin, LOW)){
-        Serial.printf("Counter has just done ------------------------------------- %d\n", counter++ );
+    if(flasher1.flash(counter, ledPin, LOW, 0)){
+        Serial.printf("Counter has just flashed %d\n", counter++ );
+        if(counter >11){
+            flasher1.enabled(false);
+            Serial.printf( "Flasher enabled = %s\n", (flasher1.enabled() ? "Yeh": "Nope"));
+            }
     };
 }
